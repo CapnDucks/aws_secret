42a9f582f75f738ea02d7f7a560db9cb8d7dfec8
Cap'n Duck <42878045+CapnDucks@users.noreply.github.com>
2023-12-22 11:35:04 -0800

    Updating var names. (#1)
    Co-authored-by: Cap'n Duck <coder@capnduck.com>

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_secretsmanager_secret_version" "this" {
   secret_id     = aws_secretsmanager_secret.this.id
   secret_string = <<EOF
     {
-      "username": "${var.admin_user}",
+      "username": "${var.db_admin_user}",
       "password": "${random_password.this.result}"
     }
   EOF
