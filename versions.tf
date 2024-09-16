22182a359161123001dd318c8ffd9803344be4bf
jelliott.io <jelliott.io@capnduck.com>
2024-09-16 15:16:52 -0700

    Actions.

--- versions.tf
+++ versions.tf
@@ -5,34 +5,26 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "5.67.0"
-
     }
     dns = {
       source  = "hashicorp/dns"
       version = "3.4.2"
-
     }
     tls = {
       source  = "hashicorp/tls"
       version = "4.0.6"
-
     }
     external = {
       source  = "hashicorp/external"
       version = ">= 2.3.4"
-
     }
     null = {
       source  = "hashicorp/null"
       version = ">= 3.2.3"
-
     }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.6.3"
-
     }
-
   }
-
 }
